c77ac4fc41770a43556adacb49f61dad1a63907a
Marco Sérvio <marcoservio22@hotmail.com>
2023-12-15 20:55:23 -0300

    Update test-nav.py

--- test-nav.py
+++ test-nav.py
@@ -1,28 +1,26 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.service import Service
 from seleniumbase import Driver
-
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import StaleElementReferenceException
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import ElementClickInterceptedException
 
 import time
 
 DIR = "/home/paulofernando1992/chromedata"
 driver = Driver(uc=True, headless=False, user_data_dir=DIR, undetectable=True)
 
+driver.get("chrome://settings/")
+driver.execute_script("chrome.settingsPrivate.setDefaultZoom(1);")
 driver.get("chrome://version/")
 driver.save_screenshot("valeuveiogarcon2.png")
 
 driver.get("chrome://settings/")
 driver.execute_script("chrome.settingsPrivate.setDefaultZoom(0.25);")
 driver.get("https://canal360i.cloud.itau.com.br/login/iparceiros")
+
+
+def fechar_cliente():
+    driver.close()
+
 
 def find_element_load(driver, by, elem):
     try:
@@ -32,6 +30,7 @@ def find_element_load(driver, by, elem):
     except:
         print("Could not find element within 30 seconds")
         fechar_cliente()
+
 
 ELEM_USERNAME_ID = "voxel-input-0"
 ELEM_PASSWORD_ID = "voxel-input-1"
