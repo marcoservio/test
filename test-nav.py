b8f9328a63a03e7dcb04c006f463d4f1f420dcdc
Marco Sérvio <marcoservio22@hotmail.com>
2023-12-15 20:36:15 -0300

    Update test-nav.py

--- test-nav.py
+++ test-nav.py
@@ -16,6 +16,10 @@ import time
 
 DIR = "/home/paulofernando1992/chromedata"
 driver = Driver(uc=True, headless=False, user_data_dir=DIR, undetectable=True)
+
+driver.get("chrome://version/")
+driver.save_screenshot("valeuveiogarcon2.png")
+
 driver.get("chrome://settings/")
 driver.execute_script("chrome.settingsPrivate.setDefaultZoom(0.25);")
 driver.get("https://canal360i.cloud.itau.com.br/login/iparceiros")
