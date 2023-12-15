05a60cf75bfdde2f76002c8bea904f422ed49102
Marco Sérvio <marcoservio22@hotmail.com>
2023-12-15 19:37:57 -0300

    Update test-nav.py

--- test-nav.py
+++ test-nav.py
@@ -1,7 +1,6 @@
-from seleniumbase import Driver
-from seleniumbase.config import settings
-from selenium.webdriver.chrome.options import Options
+DIR = "/home/paulofernando1992/chromedata"
 
+# Opções do navegador
 options = Options()
 options.add_argument("--window-size=1920,1080")
 options.add_argument("--start-maximized")
@@ -19,12 +18,25 @@ options.add_argument("--disable-setuid-sandbox")
 options.add_argument("--no-first-run")
 options.add_argument("--no-service-autorun")
 options.add_argument("--password-store=basic")
+options.add_argument(f"--user-data-dir={DIR}")
 
-DIR = "/home/paulofernando1992/chromedata"
+# Configurações específicas do seleniumbase
+self.settings.USER_DATA_DIR = DIR
 
-settings.USER_DATA_DIR = DIR
+# Configurar as opções do Chrome
+self.set_chrome_options(options)
 
-driver = Driver(uc=True, headless=False, user_data_dir=DIR, chrome_options=options)
-driver.get("chrome://version/")
-driver.save_screenshot("valeuveiogarcon2.png")
+# Inicializar o driver
+self.create_driver()
+
+# Acesse o objeto driver diretamente, se necessário
+driver = self.driver
+
+# Navegar para a URL desejada
+self.open("chrome://version/")
+
+# Capturar uma screenshot
+self.save_screenshot("valeuveiogarcon2.png")
+
+# Fechar o driver
 driver.close()
