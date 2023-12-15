022ac8fedb3ae2d68cbe647de1d9654def97032d
Marco Sérvio <marcoservio22@hotmail.com>
2023-12-15 20:26:53 -0300

    Update test-nav.py

--- test-nav.py
+++ test-nav.py
@@ -1,9 +1,7 @@
 from seleniumbase import Driver
 
 DIR = "/home/paulofernando1992/chromedata"
-driver = Driver(
-    uc=True, headless=False, user_data_dir=DIR, undetectable=True, disable_csp=True
-)
+driver = Driver(uc=True, headless=False, user_data_dir=DIR, undetectable=True)
 driver.get("chrome://version/")
 driver.save_screenshot("valeuveiogarcon2.png")
 driver.close()
