e482926b37b9791c59ab4bfa28ea60224e5c5574
Marco Sérvio <marcoservio22@hotmail.com>
2023-12-15 20:16:39 -0300

    Update test-nav.py

--- test-nav.py
+++ test-nav.py
@@ -1,30 +1,5 @@
-from seleniumbase import Driver
-from seleniumbase.config import settings
-from selenium.webdriver.chrome.options import Options
-import undetected_chromedriver as uc
-
-options = uc.ChromeOptions()
-options.add_argument("--window-size=1920,1080")
-options.add_argument("--start-maximized")
-options.add_argument("--disable-extensions")
-options.add_argument("--disable-application-cache")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--start-zoom=25")
-options.add_argument("--disable-popup-blocking")
-options.add_argument("--disable-notifications")
-options.add_argument("--disable-infobars")
-options.add_argument("--disable-extensions")
-options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-setuid-sandbox")
-options.add_argument("--no-first-run --no-service-autorun --password-store=basic")
-driver = uc.Chrome(use_subprocess=True, options=options)
-
-# Navegar para a URL desejada
+DIR = "/home/paulofernando1992/chromedata"
+driver = Driver(uc=True, headless=False, user_data_dir=DIR)
 driver.get("chrome://version/")
-
-# Capturar uma screenshot
 driver.save_screenshot("valeuveiogarcon2.png")
-
-# Fechar o driver
-driver.quit()
+driver.close()
