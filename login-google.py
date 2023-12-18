8047148e765ea4c68e3a10ce48a1dbb4267791cc
Marco Sérvio <marcoservio22@hotmail.com>
2023-12-18 12:41:13 -0300

    Update login-google.py

--- login-google.py
+++ login-google.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 DIR = "/home/paulofernando1992/chromedata"
-driver = Driver(uc=True, headless=False, user_data_dir=DIR, undetectable=True)
+driver = Driver(uc=True, headless=False, user_data_dir=DIR)
 
 driver.get("chrome://settings/")
 driver.execute_script("chrome.settingsPrivate.setDefaultZoom(1);")
